84c1b38678aea992dd60448790073c852baccbb3
Andrey <fxklim@gmail.com>
2016-04-21 15:44:28 +0000

    Верстка/стилизация searchInput

--- src/components/Search/SearchInput.js
+++ src/components/Search/SearchInput.js
@@ -12,17 +12,10 @@ export default class Search extends Component {
       }
   }
 
-  /*validationState() {
-    let length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
-  }*/
-
   handleChange() {
     // This could also be done using ReactLink:
     // http://facebook.github.io/react/docs/two-way-binding-helpers.html
-    this.props.change()
+    this.props.onChange()
     this.setState({
       value: this.refs.input.getValue()
     });
@@ -41,22 +34,34 @@ export default class Search extends Component {
   }
 
   render() {
-    const groupCls = cs({
-      'search-input': true,
-      active: this.state.active
-    })
+    const rootCls = cs({
+        'search-input': true,
+        active: this.state.active
+      }),
+      groupCls = cs({
+        __input: true,
+        active: this.state.active
+      }),
+      bgCls = cs({
+        __bg: true,
+        active: this.state.active
+      })
     
     return (
-      <Input
-        type='text'
-        groupClassName={groupCls}
-        value={this.state.value}
-        placeholder='Search'
-        bsSize='small'
-        ref='input'
-        onFocus={() => this.handleFocus()}
-        onBlur={() => this.handleBlur()}
-        onChange={() => this.handleChange()} />
+      <div className={rootCls}>
+        <div className={bgCls}></div>
+        <div className='__tags'></div>
+        <Input
+          type='text'
+          groupClassName={groupCls}
+          value={this.state.value}
+          placeholder='Search'
+          bsSize='small'
+          ref='input'
+          onFocus={() => this.handleFocus()}
+          onBlur={() => this.handleBlur()}
+          onChange={() => this.handleChange()} />
+      </div>
     );
   }
 }
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -7,7 +7,7 @@ import SearchPanel from '../components/Search/SearchPanel'
 export default class Search extends Component {
     render () {
         return <div className='search'>
-            <SearchInput change={this.props.actions.getGenres} />
+            <SearchInput onChange={this.props.actions.getGenres} />
             <SearchPanel search={this.props.search} />
         </div>
     }
