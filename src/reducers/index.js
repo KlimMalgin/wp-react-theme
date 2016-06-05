704eb177e14d932643e4f35a24fd4133d0637356
Andrey <fxklim@gmail.com>
2016-06-05 19:26:03 +0000

    remove old code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import page from './page'
 import user from './user'
 import search from './search'
 import shadow from './shadow'
 
 export default combineReducers({
-  page,
   user,
   search,
   shadow,
